a77e195a489bdf121f52a2fa9ef0b9324174cdc5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-05-10 14:54:06 +0530

    feat: make a barebones buy me a coffee app

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { Toaster } from "react-hot-toast";
+import "../styles/globals.css";
+import React from "react";
+
+const theme = createTheme({ type: "dark" });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={theme}>
+      <Component {...pageProps} />
+      <Toaster
+        toastOptions={{
+          style: { color: "#ffffff", background: "#333333" },
+          position: "bottom-right",
+        }}
+      />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
